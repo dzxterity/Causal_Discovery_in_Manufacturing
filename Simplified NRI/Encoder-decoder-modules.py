617c2ab2724f896a31742b04bae4c4f5205bf8de
dzxterity <dzxterity@gmail.com>
2023-07-28 22:23:46 +0300

    Add files via upload

---
+++ Simplified NRI/Encoder-decoder-modules.py
@@ -0,0 +1,285 @@
+import pandas as pd
+import torch
+import torch.nn as nn
+import numpy as np
+import torch.nn.functional as F
+from torch.autograd import Variable
+from torch.optim import Adam
+
+class MLPDecoder(nn.Module):
+    """MLP decoder module."""
+
+    def __init__(self, n_in_node, edge_types, msg_hid, msg_out, n_hid,
+                 do_prob=0., skip_first=False):
+        super(MLPDecoder, self).__init__()
+        self.msg_fc1 = nn.ModuleList(
+            [nn.Linear(2 * n_in_node, msg_hid) for _ in range(edge_types)])
+        self.msg_fc2 = nn.ModuleList(
+            [nn.Linear(msg_hid, msg_out) for _ in range(edge_types)])
+        self.msg_out_shape = msg_out
+        self.skip_first_edge_type = skip_first
+
+        self.out_fc1 = nn.Linear(n_in_node + msg_out, n_hid)
+        self.out_fc2 = nn.Linear(n_hid, n_hid)
+        self.out_fc3 = nn.Linear(n_hid, n_in_node)
+
+        print('Using learned interaction net decoder.')
+
+        self.dropout_prob = do_prob
+
+    def single_step_forward(self, single_timestep_inputs, rel_rec, rel_send,
+                            single_timestep_rel_type):
+
+        # single_timestep_inputs has shape
+        # [batch_size, num_timesteps, num_atoms, num_dims]
+
+        # single_timestep_rel_type has shape:
+        # [batch_size, num_timesteps, num_atoms*(num_atoms-1), num_edge_types]
+
+        # Node2edge
+        receivers = torch.matmul(rel_rec, single_timestep_inputs)
+        senders = torch.matmul(rel_send, single_timestep_inputs)
+        pre_msg = torch.cat([senders, receivers], dim=-1)
+
+        all_msgs = Variable(torch.zeros(pre_msg.size(0), pre_msg.size(1),
+                                        pre_msg.size(2), self.msg_out_shape))
+        if single_timestep_inputs.is_cuda:
+            all_msgs = all_msgs.cuda()
+
+        if self.skip_first_edge_type:
+            start_idx = 1
+        else:
+            start_idx = 0
+
+        # Run separate MLP for every edge type
+        # NOTE: To exlude one edge type, simply offset range by 1
+        for i in range(start_idx, len(self.msg_fc2)):
+            msg = F.relu(self.msg_fc1[i](pre_msg))
+            msg = F.dropout(msg, p=self.dropout_prob)
+            msg = F.relu(self.msg_fc2[i](msg))
+            msg = msg * single_timestep_rel_type[:, :, :, i:i + 1]
+            all_msgs += msg
+
+        # Aggregate all msgs to receiver
+        agg_msgs = all_msgs.transpose(-2, -1).matmul(rel_rec).transpose(-2, -1)
+        agg_msgs = agg_msgs.contiguous()
+
+        # Skip connection
+        aug_inputs = torch.cat([single_timestep_inputs, agg_msgs], dim=-1)
+
+        # Output MLP
+        pred = F.dropout(F.relu(self.out_fc1(aug_inputs)), p=self.dropout_prob)
+        pred = F.dropout(F.relu(self.out_fc2(pred)), p=self.dropout_prob)
+        pred = self.out_fc3(pred)
+
+        # Predict position/velocity difference
+        return single_timestep_inputs + pred
+
+    def forward(self, inputs, rel_type, rel_rec, rel_send, pred_steps=1):
+        # NOTE: Assumes that we have the same graph across all samples.
+
+        inputs = inputs.transpose(1, 2).contiguous()
+
+        sizes = [rel_type.size(0), inputs.size(1), rel_type.size(1),
+                 rel_type.size(2)]
+        rel_type = rel_type.unsqueeze(1).expand(sizes)
+
+        time_steps = inputs.size(1)
+        assert (pred_steps <= time_steps)
+        preds = []
+
+        # Only take n-th timesteps as starting points (n: pred_steps)
+        last_pred = inputs[:, 0::pred_steps, :, :]
+        curr_rel_type = rel_type[:, 0::pred_steps, :, :]
+        # NOTE: Assumes rel_type is constant (i.e. same across all time steps).
+
+        # Run n prediction steps
+        for step in range(0, pred_steps):
+            last_pred = self.single_step_forward(last_pred, rel_rec, rel_send,
+                                                 curr_rel_type)
+            preds.append(last_pred)
+
+        sizes = [preds[0].size(0), preds[0].size(1) * pred_steps,
+                 preds[0].size(2), preds[0].size(3)]
+
+        output = Variable(torch.zeros(sizes))
+        if inputs.is_cuda:
+            output = output.cuda()
+
+        # Re-assemble correct timeline
+        for i in range(len(preds)):
+            output[:, i::pred_steps, :, :] = preds[i]
+
+        pred_all = output[:, :(inputs.size(1) - 1), :, :]
+
+        return pred_all.transpose(1, 2).contiguous()
+
+# Generate off-diagonal interaction graph
+off_diag = np.ones([52, 52]) - np.eye(52)
+
+rel_rec = np.array(encode_onehot(np.where(off_diag)[0]), dtype=np.float32)
+rel_send = np.array(encode_onehot(np.where(off_diag)[1]), dtype=np.float32)
+rel_rec = torch.FloatTensor(rel_rec)
+rel_send = torch.FloatTensor(rel_send)
+
+class MLPEncoder(nn.Module):
+    def __init__(self, n_in, n_hid, n_out, do_prob=0., factor=True):
+        super(MLPEncoder, self).__init__()
+
+        self.factor = factor
+
+        self.mlp1 = MLP(n_in, n_hid, n_hid, do_prob)
+        self.mlp2 = MLP(n_hid * 2, n_hid, n_hid, do_prob)
+        self.mlp3 = MLP(n_hid, n_hid, n_hid, do_prob)
+        if self.factor:
+            self.mlp4 = MLP(n_hid * 3, n_hid, n_hid, do_prob)
+            print("Using factor graph MLP encoder.")
+        else:
+            self.mlp4 = MLP(n_hid * 2, n_hid, n_hid, do_prob)
+            print("Using MLP encoder.")
+        self.fc_out = nn.Linear(n_hid, n_out)
+        self.init_weights()
+
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_normal(m.weight.data)
+                m.bias.data.fill_(0.1)
+
+    def edge2node(self, x, rel_rec, rel_send):
+        # NOTE: Assumes that we have the same graph across all samples.
+        incoming = torch.matmul(rel_rec.t(), x)
+        return incoming / incoming.size(1)
+
+    def node2edge(self, x, rel_rec, rel_send):
+        # NOTE: Assumes that we have the same graph across all samples.
+        receivers = torch.matmul(rel_rec, x)
+        senders = torch.matmul(rel_send, x)
+        edges = torch.cat([senders, receivers], dim=2)
+        return edges
+
+    def forward(self, inputs, rel_rec, rel_send):
+        # Input shape: [num_sims, num_atoms, num_timesteps, num_dims]
+        x = inputs #.view(inputs.size(0), inputs.size(1), -1)
+        # New shape: [num_sims, num_atoms, num_timesteps*num_dims]
+
+        x = self.mlp1(x)  # 2-layer ELU net per node
+
+        x = self.node2edge(x, rel_rec, rel_send)
+        x = self.mlp2(x)
+        x_skip = x
+
+        if self.factor:
+            x = self.edge2node(x, rel_rec, rel_send)
+            x = self.mlp3(x)
+            x = self.node2edge(x, rel_rec, rel_send)
+            x = torch.cat((x, x_skip), dim=2)  # Skip connection
+            x = self.mlp4(x)
+        else:
+            x = self.mlp3(x)
+            x = torch.cat((x, x_skip), dim=2)  # Skip connection
+            x = self.mlp4(x)
+
+        return self.fc_out(x)
+
+    # енкодер:
+
+    def my_softmax(input, axis=1):
+        trans_input = input.transpose(axis, 0).contiguous()
+        soft_max_1d = F.softmax(trans_input)
+        return soft_max_1d.transpose(axis, 0)
+
+    def sample_gumbel(shape, eps=1e-10):
+        """
+        NOTE: Stolen from https://github.com/pytorch/pytorch/pull/3341/commits/327fcfed4c44c62b208f750058d14d4dc1b9a9d3
+
+        Sample from Gumbel(0, 1)
+
+        based on
+        https://github.com/ericjang/gumbel-softmax/blob/3c8584924603869e90ca74ac20a6a03d99a91ef9/Categorical%20VAE.ipynb ,
+        (MIT license)
+        """
+        U = torch.rand(shape).float()
+        return - torch.log(eps - torch.log(U + eps))
+
+    def gumbel_softmax_sample(logits, tau=1, eps=1e-10):
+        """
+        NOTE: Stolen from https://github.com/pytorch/pytorch/pull/3341/commits/327fcfed4c44c62b208f750058d14d4dc1b9a9d3
+
+        Draw a sample from the Gumbel-Softmax distribution
+
+        based on
+        https://github.com/ericjang/gumbel-softmax/blob/3c8584924603869e90ca74ac20a6a03d99a91ef9/Categorical%20VAE.ipynb
+        (MIT license)
+        """
+        gumbel_noise = sample_gumbel(logits.size(), eps=eps)
+        if logits.is_cuda:
+            gumbel_noise = gumbel_noise.cuda()
+        y = logits + Variable(gumbel_noise)
+        return my_softmax(y / tau, axis=-1)
+
+    def gumbel_softmax(logits, tau=1, hard=False, eps=1e-10):
+        """
+        NOTE: Stolen from https://github.com/pytorch/pytorch/pull/3341/commits/327fcfed4c44c62b208f750058d14d4dc1b9a9d3
+
+        Sample from the Gumbel-Softmax distribution and optionally discretize.
+        Args:
+          logits: [batch_size, n_class] unnormalized log-probs
+          tau: non-negative scalar temperature
+          hard: if True, take argmax, but differentiate w.r.t. soft sample y
+        Returns:
+          [batch_size, n_class] sample from the Gumbel-Softmax distribution.
+          If hard=True, then the returned sample will be one-hot, otherwise it will
+          be a probability distribution that sums to 1 across classes
+
+        Constraints:
+        - this implementation only works on batch_size x num_features tensor for now
+
+        based on
+        https://github.com/ericjang/gumbel-softmax/blob/3c8584924603869e90ca74ac20a6a03d99a91ef9/Categorical%20VAE.ipynb ,
+        (MIT license)
+        """
+        y_soft = gumbel_softmax_sample(logits, tau=tau, eps=eps)
+        if hard:
+            shape = logits.size()
+            _, k = y_soft.data.max(-1)
+            # this bit is based on
+            # https://discuss.pytorch.org/t/stop-gradients-for-st-gumbel-softmax/530/5
+            y_hard = torch.zeros(*shape)
+            if y_soft.is_cuda:
+                y_hard = y_hard.cuda()
+            y_hard = y_hard.zero_().scatter_(-1, k.view(shape[:-1] + (1,)), 1.0)
+            # this cool bit of code achieves two things:
+            # - makes the output value exactly one-hot (since we add then
+            #   subtract y_soft value)
+            # - makes the gradient equal to y_soft gradient (since we strip
+            #   all other gradients)
+            y = Variable(y_hard - y_soft.data) + y_soft
+        else:
+            y = y_soft
+        return y
+
+
+def nll_gaussian(preds, target, variance, add_const=False):
+    neg_log_p = ((preds - target) ** 2 / (2 * variance))
+    if add_const:
+        const = 0.5 * np.log(2 * np.pi * variance)
+        neg_log_p += const
+    return neg_log_p.sum() / (target.size(0) * target.size(1))
+
+def kl_categorical(preds, log_prior, num_atoms, eps=1e-16):
+    kl_div = preds * (torch.log(preds + eps) - log_prior)
+    return kl_div.sum() / (num_atoms * preds.size(0))
+
+
+def kl_categorical_uniform(preds, num_atoms, num_edge_types, add_const=False,
+                           eps=1e-16):
+    kl_div = preds * torch.log(preds + eps)
+    if add_const:
+        const = np.log(num_edge_types)
+        kl_div += const
+    return kl_div.sum() / (num_atoms * preds.size(0))
+
+
+
+
